44d457d11e0be590dfdead8f2c50accf0f56857a
Arden Choi <ardenyschoi@gmail.com>
2023-01-21 23:53:44 -0800

    added js file to stats_page

--- stats.js
+++ stats.js
@@ -17,31 +17,35 @@ for (let i=0; i<items.length; i++) {
 var scores = document.getElementsByClassName("number-this");
 
 scores[0].innerHTML = 1 + "<span style=\"font-size: medium;\">/5</span>";
-scores[1].innerText = 100 + "%";
+scores[1].innerText = 100 + "g";
 
-var scoreChange = [-10, 10];
+var scoreChange = [10, 10];
 var descriptions = document.getElementsByClassName("description");
 let novaScore = document.getElementById("change-nova");
 let carbonScore = document.getElementById("change-carbon");
 let good = "<div><img src=\"https://img.icons8.com/ios/100/40C057/happy--v1.png\"></div><span style=\"color:#00c348\";>less </span>";
 let bad = "<div><img src=\"https://img.icons8.com/ios/100/FA5252/sad.png\"/></div><span style=\"color:#e73212\";>more </span>";
 // nova score
-// if (scoreChange[0] < 0) {
-//     novaScore.className = "number-compare-good";
-//     novaScore.innerText = "↓ " + String(scoreChange[0]) + "%";
-//     descriptions[0].innerHTML = good + "processed food in your diet";
-// } else if (scoreChange[0] > 0) {
-//     novaScore.className = "number-compare-bad";
-//     novaScore.innerText = "↑ " + String(scoreChange[0]) + "%";
-//     descriptions[0].innerHTML = bad + "processed food in your diet";
-// } 
-// // carbon score
-// if (scoreChange[1] < 0) {
-//     carbonScore.className = "number-compare-good";
-//     carbonScore.innerText = "↓ " + String(scoreChange[1]) + "%";
-//     descriptions[1].innerHTML = good + "carbon footprint emitted";
-// } else if (scoreChange[1] > 0) {
-//     carbonScore.className = "number-compare-bad";
-//     carbonScore.innerText = "↓ " + String(scoreChange[1]) + "%";
-//     descriptions[1].innerHTML = bad + "carbon footprint emitted";
-// } 
+if (scoreChange[0] < 0) {
+    novaScore.className = "number-compare-good";
+    novaScore.innerText = "↓ " + String(scoreChange[0]*(-1)) + "%";
+    descriptions[0].innerHTML = good + "processed food in your diet";
+} else if (scoreChange[0] > 0) {
+    novaScore.className = "number-compare-bad";
+    novaScore.innerText = "↑ " + String(scoreChange[0]) + "%";
+    descriptions[0].innerHTML = bad + "processed food in your diet";
+} else {
+
+}
+// carbon score
+if (scoreChange[1] < 0) {
+    carbonScore.className = "number-compare-good";
+    carbonScore.innerText = "↓ " + String(scoreChange[1]*(-1)) + "%";
+    descriptions[1].innerHTML = good + "carbon footprint emitted";
+} else if (scoreChange[1] > 0) {
+    carbonScore.className = "number-compare-bad";
+    carbonScore.innerText = "↑ " + String(scoreChange[1]) + "%";
+    descriptions[1].innerHTML = bad + "carbon footprint emitted";
+} else {
+
+}
