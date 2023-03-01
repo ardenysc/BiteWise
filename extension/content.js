d1ea4d9480ddf32826e5aee431d5e0f0cfa381a8
Christopher-Eunho <christopher.eunho@gmail.com>
2023-02-28 22:03:06 -0800

    clean up code

--- extension/content.js
+++ extension/content.js
@@ -9,27 +9,25 @@ const updateCoords = (event) => {
     yCoord = event.clientY;
 } 
 
+const handleOnMessage = (msg, sender, sendResponse) => {
+    console.log("here")
+    var popup = document.createElement('div');
+    popup.style.position = 'fixed';
+    popup.style.left =  xCoord + 'px';
+    popup.style.top =  yCoord + 'px';
+    popup.style.padding = '10px';
+    popup.style.zIndex = '9999';
+    popup.style.background = 'white';
+    popup.className = "popup";
+    popup.style.border = '1px solid gray';
+    popup.innerHTML = `<b>Product Name: ${msg['name']}</b><br>\
+                        <b>Brand: ${msg['brand']}</b><br>\
+                        <b>Grade: ${msg['grade']}</b><br>\
+                        <b>Nova Score: ${msg['nova']}</b><br>\
+                        <b>Co2 Emission: ${msg['co2']}</b>`;
+    document.body.appendChild(popup);
+    console.log(msg);
+}
+
 document.addEventListener("contextmenu", updateCoords);
-
-chrome.runtime.onMessage.addListener(
-    function(message, sender, sendResponse) {
-        console.log("here")
-
-        var popup = document.createElement('div');
-        popup.style.position = 'fixed';
-        popup.style.left =  xCoord + 'px';
-        popup.style.top =  yCoord + 'px';
-        popup.style.padding = '10px';
-        popup.style.zIndex = '9999';
-        popup.style.background = 'white';
-        popup.className = "popup";
-        popup.style.border = '1px solid gray';
-        popup.innerHTML = `<b>Product Name: ${message['name']}</b><br>\
-                           <b>Brand: ${message['brand']}</b><br>\
-                           <b>Grade: ${message['grade']}</b><br>\
-                           <b>Nova Score: ${message['nova']}</b><br>\
-                           <b>Co2 Emission: ${message['co2']}</b>`;
-        document.body.appendChild(popup);
-
-        console.log(message);
-})
+chrome.runtime.onMessage.addListener(handleOnMessage);
