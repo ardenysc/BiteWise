8d918a98888a70bc9197c8d9fdf4201f0ab37eb0
Christopher-Eunho <christopher.eunho@gmail.com>
2023-03-01 21:25:58 -0800

    server and basic middleware setup

--- src/server.js
+++ src/server.js
@@ -1,6 +1,17 @@
 import express from "express";
+import morgan from "morgan"; // logging middleware
+const getRoot = (req, res) => {
+    console.log(req);
+    res.send('Hello World');
+    // if you just kill the request, you can also res.end();
+
+}
 
 const app = express();
+const logger = morgan("dev"); //"dev": config option
+
 app.use(express.urlencoded({ extended: true }));
+app.use(logger); // logging middleware
+app.get("/",getRoot);
 
 export default app;
